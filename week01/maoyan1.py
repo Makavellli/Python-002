0d5f14b4f0cc8de7d95f70214fd7a80a0727a88d
Makaveli <makavelli@163.com>
2020-07-27 01:21:51 +0800

    猫眼电影信息爬取程序修改20200727-1

--- week01/maoyan1.py
+++ week01/maoyan1.py
@@ -1,6 +1,8 @@
-# 我的而第一个爬虫程序
+# 我的而第一个爬虫程序 获取猫眼电影按评分排名前10的电影基本信息
 import requests
 from bs4 import BeautifulSoup as bs
+import pandas as pd
+
 
 ##指定访问客户端
 user_agent = 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36'
@@ -15,21 +17,27 @@ response = requests.get(myurl,headers=header)
 ## 处理文文本信息
 bs_info = bs(response.text, 'html.parser')
 
-##调试信息打印
-##print(bs_info)
-
 # 通过for循环找到指定html节点，遍历获取电影基本信息
 topNo=0
-filmUrl=''
-filmId=''
+filmName=''
+list=[]
+# 定位电影基本信息位置
 for tags in bs_info.find_all('div', attrs={'class': 'movie-hover-info'}):
-    topNo = topNo + 1
-    for atag in tags.find_all('div'):
-        # 获取电影名字
-        atag.find_all('div', attrs={'class': 'movie-hover-title'})
-        if topNo==2:
-            break
-        print('序号:' + str(topNo))
+    topNo+=1
+# 遍历电影基本信息节点获取
+    for info in tags.find_all('div', attrs={'class': 'movie-hover-title'}):
+        filmName=info.text
+        list.append(filmName.replace(' ', ''))
+        print(filmName.replace(' ', ''))
+
+    print('当前序号是：'+str(topNo))
+# 判断当前为第几个 第10个的时候跳出循环
+    if topNo == 10:
+        break
     else:
         continue
     break
+
+movie1 = pd.DataFrame(data = list)
+# 保存为csv文件
+movie1.to_csv('./films.csv', encoding='GBK', index=False, header=False)
